5fccca8185ba92e2d93a932d5afabc7edeceb2e7
leeseojune <sung07288346@gmail.com>
2021-12-17 22:57:24 +0900

    ♻ :: 코드 간결화 및 규칙 준수

--- src/main/kotlin/io/github/spotiparty/server/domain/room/domain/Room.kt
+++ src/main/kotlin/io/github/spotiparty/server/domain/room/domain/Room.kt
@@ -1,5 +1,6 @@
 package io.github.spotiparty.server.domain.room.domain
 
+import io.github.spotiparty.server.global.domain.BaseUUIDEntity
 import org.hibernate.annotations.GenericGenerator
 import javax.persistence.Column
 import javax.persistence.Entity
@@ -8,12 +9,6 @@ import javax.persistence.Id
 
 @Entity
 class Room(
-    
-    @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(36)")
-    val uuid: String,
 
     @Column(columnDefinition = "BIT(1)", nullable = false)
     val isPublic: Boolean,
@@ -21,4 +16,4 @@ class Room(
     @Column(columnDefinition = "INT", nullable = false)
     val headCount: Int
 
-)
+) : BaseUUIDEntity()
