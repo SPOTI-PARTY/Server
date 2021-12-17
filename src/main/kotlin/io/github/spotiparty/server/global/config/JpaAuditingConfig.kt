ff7f968612cfc9ea461e6cc4356ca62211786d02
leeseojune <sung07288346@gmail.com>
2021-12-17 23:21:17 +0900

    📑 :: JpaAuditingConfig

---
+++ src/main/kotlin/io/github/spotiparty/server/global/config/JpaAuditingConfig.kt
@@ -0,0 +1,9 @@
+package io.github.spotiparty.server.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaAuditingConfig {
+}
