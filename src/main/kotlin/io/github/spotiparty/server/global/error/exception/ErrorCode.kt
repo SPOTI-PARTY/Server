26e481d0cac6ce36039ee05c54230e5da29f4f8b
leeseojune <sung07288346@gmail.com>
2021-12-17 23:36:31 +0900

    📑 :: Error enum

---
+++ src/main/kotlin/io/github/spotiparty/server/global/error/exception/ErrorCode.kt
@@ -0,0 +1,14 @@
+package io.github.spotiparty.server.global.error.exception
+
+import com.fasterxml.jackson.annotation.JsonFormat
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+enum class ErrorCode(
+    val status: Int,
+    val message: String
+) {
+
+    BAD_REQUEST(400, "Bad request."),
+    NOT_FOUND(404, "Not found.")
+
+}
