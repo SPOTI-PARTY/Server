6bc922481d4dc833828619743addad994e70d9fd
leeseojune <sung07288346@gmail.com>
2021-12-18 00:08:34 +0900

    📑 :: findByEmail

--- src/main/kotlin/io/github/spotiparty/server/domain/user/domain/repositories/UserRepository.kt
+++ src/main/kotlin/io/github/spotiparty/server/domain/user/domain/repositories/UserRepository.kt
@@ -4,4 +4,5 @@ import io.github.spotiparty.server.domain.user.domain.User
 import org.springframework.data.repository.CrudRepository
 
 interface UserRepository: CrudRepository<User, String> {
+    fun findByEmail(id: String): User?
 }
