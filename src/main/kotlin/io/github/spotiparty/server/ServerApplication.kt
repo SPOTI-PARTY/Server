6fc998188679b3f2ae5eacd217f401c62727ae1d
leeseojune <sung07288346@gmail.com>
2021-12-18 01:12:14 +0900

    ⚡ :: ConfigurationPropertiesScan

--- src/main/kotlin/io/github/spotiparty/server/ServerApplication.kt
+++ src/main/kotlin/io/github/spotiparty/server/ServerApplication.kt
@@ -1,8 +1,12 @@
 package io.github.spotiparty.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class ServerApplication
 
